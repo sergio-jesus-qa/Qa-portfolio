3951bf3f4aaa1140aa20ab9f6ca15f71b6fa3ee0
Sérgio Jesus <sergido102@gmail.com>
2025-12-20 00:23:50 +0000

    Adicionado projeto Cypress com testes de login

---
+++ automacao-cypress/cypress/e2e/login.spec.js
@@ -0,0 +1,28 @@
+describe('Testes de Login no SauceDemo', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+  })
+
+  it('Login válido - standard_user', () => {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.url().should('include', '/inventory.html')
+    cy.get('.title').should('contain', 'Products')
+  })
+
+  it('Login inválido', () => {
+    cy.get('[data-test="username"]').type('usuario_errado')
+    cy.get('[data-test="password"]').type('senha_errada')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('contain', 'Username and password do not match')
+  })
+
+  it('Usuário bloqueado', () => {
+    cy.get('[data-test="username"]').type('locked_out_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('contain', 'Sorry, this user has been locked out')
+  })
+})
